564f516b4efa4c8817de9b52c63ae7ae234c1f75
Jraviolo <josefina.raviolo@gmail.com>
2019-08-29 22:12:54 -0300

    inicial

--- app/src/main/java/tp/sendmeal/MainActivity.java
+++ app/src/main/java/tp/sendmeal/MainActivity.java
@@ -3,12 +3,46 @@ package tp.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText nombre;
+    private EditText clave;
+    private EditText clave2;
+    private EditText correo;
+    private EditText tarjeta;
+    private EditText ccv;
+    private EditText vtoTarjeta;
+    private RadioGroup groupTipoCuenta;
+    private String tipoCuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnRegistrar = (Button) findViewById(R.id.AMbtnRegistrar);
+        btnRegistrar.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        nombre = (EditText) findViewById(R.id.AMeditNombre);
+        clave = (EditText)findViewById(R.id.AMeditClave);
+        clave2 = (EditText)findViewById(R.id.AMeditClave2);
+        correo = (EditText)findViewById(R.id.AMeditCorreo);
+        tarjeta = (EditText)findViewById(R.id.AMeditNumero);
+        ccv = (EditText)findViewById(R.id.AMeditDig);
+        vtoTarjeta = (EditText)findViewById(R.id.AMeditVto);
+        groupTipoCuenta = (RadioGroup) findViewById(R.id.AMTipoCuenta);
+        tipoCuenta = ((RadioButton) findViewById(groupTipoCuenta.getCheckedRadioButtonId())).getText().toString();
+
+    }
+    
 }
