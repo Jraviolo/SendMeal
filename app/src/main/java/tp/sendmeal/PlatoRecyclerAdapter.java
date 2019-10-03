3e1aedadaacde7bc61444d40d9aa17d08bc30238
aleeevola <aleeevola@gmail.com>
2019-10-03 19:53:33 -0300

    onClick

--- app/src/main/java/tp/sendmeal/PlatoRecyclerAdapter.java
+++ app/src/main/java/tp/sendmeal/PlatoRecyclerAdapter.java
@@ -1,22 +1,25 @@
 package tp.sendmeal;
 
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.net.URI;
 import java.util.List;
 
 import tp.sendmeal.domain.Plato;
 
+import static android.app.PendingIntent.getActivity;
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+import static androidx.core.content.ContextCompat.startActivity;
+
 public class PlatoRecyclerAdapter extends RecyclerView.Adapter<PlatoRecyclerAdapter.PlatoViewHolder> {
-    private List<Plato> mDataSet;
+    public List<Plato> mDataSet;
+
 
 
     public PlatoRecyclerAdapter (List<Plato> myDataSet){
@@ -36,6 +39,8 @@ public class PlatoRecyclerAdapter extends RecyclerView.Adapter<PlatoRecyclerAdap
         holder.nombrePlato.setText(plato.getTitulo());
         holder.precio.setText("$ "+plato.getPrecio().toString());
 
+        //define que elemento del array es
+        holder.btnEditar.setTag(position);
     }
 
     @Override
@@ -48,12 +53,34 @@ public class PlatoRecyclerAdapter extends RecyclerView.Adapter<PlatoRecyclerAdap
         TextView nombrePlato;
         TextView precio;
 
+        Button btnEditar;
+
+        private static final int CODIGO_LISTA_PLATO = 987;
+
         PlatoViewHolder(View base){
             super(base);
             this.imagenPlato = (ImageView) base.findViewById(R.id.FPimagen);
             this.nombrePlato = (TextView) base.findViewById(R.id.FPnombrePlato);
             this.precio = (TextView) base.findViewById(R.id.FPprecio);
 
+            this.btnEditar=(Button) base.findViewById(R.id.FPbtnEditar);
+
+
+            this.btnEditar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    /*Intent i = new Intent(,AltaPlato.class);
+                    i.putExtra("INDICE",(int)btnEditar.getTag());
+                    startActivityForResult(i,CODIGO_LISTA_PLATO);*/
+                }
+            });
+
+            /*this.btnEditar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("Editar: "+btnEditar.getTag().toString());
+                }
+            });*/
         }
 
     }
